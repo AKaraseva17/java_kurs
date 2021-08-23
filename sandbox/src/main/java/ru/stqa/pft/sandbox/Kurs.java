64e7ff5733a7eeb14746dd487db0a82a00a320af
AKaraseva17 <badsanta767676@gmail.com>
2021-08-23 15:32:30 +0300

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Kurs.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Kurs.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class Kurs {
 
   public static void main(String[] args) {
-    System.out.println("Hellow, world!");
+    hello("world");
+    hello("user");
+    hello("Anastasia");
+
+    double l = 5;
+    System.out.println("площадь квадрата со стороной" + l + "=" + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("площадь прямоугольника со сторонами" + a + "и" + b+ area(a,b));
+
   }
-  
-}
+    public static void hello(String somebody) {
+      System.out.println("Hellow," + somebody + "!");
+    }
+    public static double area(double len) {
+    return len * len;
+    }
+    public static double area(double a, double b) {
+    return a * b;
+    }
+  }
+
