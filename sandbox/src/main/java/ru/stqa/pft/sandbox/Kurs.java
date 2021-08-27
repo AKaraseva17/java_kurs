c3f0939fd589ab0f23134c2850c81096ff331d6e
AKaraseva17 <badsanta767676@gmail.com>
2021-08-27 16:25:39 +0300

    перенос функции внутрь класса. (Превращение функции в метод)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Kurs.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Kurs.java
@@ -15,14 +15,12 @@ public class Kurs {
 
     Point p1 = new Point(90,67);
     Point p2 = new Point(78,43);
-    System.out.println("Расстояние между двумя точками p1 и p2 " + "= " +  distance(p1,p2));
+    System.out.println("Расстояние между двумя точками p1 и p2 " + "= " +  p1.distance(p2));
   }
     public static void hello(String somebody) {
       System.out.println("Hello," + somebody + "!");
     }
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(((p2.x-p1.x) * (p2.x- p1.x)) + ((p2.y- p1.y) * (p2.y- p1.y)));
-  }
+
 
 }
 
