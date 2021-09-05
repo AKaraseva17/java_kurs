e206e32e747b0dc7429a100c550a37f155f44dd8
AKaraseva17 <badsanta767676@gmail.com>
2021-09-05 15:32:01 +0300

    Добавление различных браузеров для запуска тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,7 +7,7 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
-    app.getContactHelper().initIDContactModification("14");
+    app.getContactHelper().initIDContactModification("11");
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("Понин", "Василий", "kotovic@mail.ru", "87777666666"));
     app.getContactHelper().outputContactModificationForm();
