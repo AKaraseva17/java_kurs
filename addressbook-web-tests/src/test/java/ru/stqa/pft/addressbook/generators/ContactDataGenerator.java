aed503de5bae10a1152c4e0c0eba256194947be6
AKaraseva17 <badsanta767676@gmail.com>
2021-10-09 17:04:09 +0300

    Автоматическое закрытие файлов после использования для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -19,10 +19,10 @@ public class ContactDataGenerator {
   @Parameter(names = "-c", description = "Contact count")
   public int count;
 
-  @Parameter(names = "-f",description = "Target file")
+  @Parameter(names = "-f", description = "Target file")
   public String file;
 
-  @Parameter(names = "-d",description = "Data format")
+  @Parameter(names = "-d", description = "Data format")
   public String format;
 
   public static void main(String[] args) throws IOException {
@@ -40,11 +40,11 @@ public class ContactDataGenerator {
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
     if (format.equals("csv")) {
-      saveAsCsv(contacts,new File(file));
-    } else if (format.equals("xml")){
-      saveAsXml(contacts,new File(file));
-    } else if (format.equals("json")){
-      saveAsJson(contacts,new File(file));
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(contacts, new File(file));
     } else {
       System.out.println("Unrecognized format " + format);
     }
@@ -54,26 +54,27 @@ public class ContactDataGenerator {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
+
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format
-              ("%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
-                      contact.getAddress(), contact.getEmail(), contact.getHomePhone()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format
+                ("%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
+                        contact.getAddress(), contact.getEmail(), contact.getHomePhone()));
+      }
     }
-    writer.close();
   }
 
   private List<ContactData> generateContacts(int count) {
