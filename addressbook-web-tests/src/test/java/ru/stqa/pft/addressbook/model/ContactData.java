887120db992e404ec3da89fe23dedad5bff95aad
AKaraseva17 <badsanta767676@gmail.com>
2021-10-10 14:39:47 +0300

    DbConnectionTest для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,31 +3,67 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstname")
   private String firstname;
+
   @Expose
+  @Column(name = "lastname")
   private String lastname;
+
   @Expose
+  @Column(name = "email")
   private String email;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email2;
+
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email3;
+
+  @Transient
   private String allPhones;
+  @Transient
   private String allEmails;
+
   @Expose
+  @Type(type = "text")
+  @Column(name = "address")
   private String address;
-  private File photo;
+
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public String getAddress() {
     return address;
@@ -41,11 +77,11 @@ public class ContactData {
 
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
@@ -8,8 +10,8 @@ import java.sql.*;
 
 public class DbConnectionTest {
 
-  @Test
-  public void testDbConnection(){
+  @Test(enabled = false)
+  public void testGroupDbConnection(){
     Connection conn = null;
     try {
       conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
@@ -25,6 +27,32 @@ public class DbConnectionTest {
       conn.close();
 
       System.out.println(groups);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+  @Test(enabled = true)
+  public void testContactDbConnection(){
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select id,firstname,lastname,email,home,address from addressbook");
+      Contacts contacts = new Contacts();
+      while (rs.next()) {
+        contacts.add(new ContactData().withId(rs.getInt("id")).withFirstname(rs.getString("firstname"))
+                .withLastname(rs.getString("lastname")).withAddress(rs.getString("address"))
+                .withEmail(rs.getString("email")).withHomePhone(rs.getString("home")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+
+      System.out.println(contacts);
 
     } catch (SQLException ex) {
       // handle any errors
